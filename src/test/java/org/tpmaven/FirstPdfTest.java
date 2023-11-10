ff9ab43d4b78e40a379469b36df4761cfc52bef5
kilian.marcelin@gmail.com <kilian.marcelin@askoed.com>
2023-11-10 08:50:10 +0100

    Initial commit

---
+++ src/test/java/org/tpmaven/FirstPdfTest.java
@@ -0,0 +1,32 @@
+package org.tpmaven;
+
+import junit.framework.TestCase;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FirstPdfTest extends TestCase {
+
+    public FirstPdfTest(String testName){
+        super(testName);
+    }
+
+    @Test
+    void main() {
+        assertTrue(true);
+    }
+
+    @Test
+    void addMetaData() {
+    }
+
+    @Test
+    void addTitlePage() {
+
+    }
+
+    @Test
+    void addContent() {
+    }
+}
